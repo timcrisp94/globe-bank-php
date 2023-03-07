0887d2a2f58fccf3b63f5cc9963846658176e69d
Tim Crisp <timothycrisp@gmail.com>
2023-03-07 14:49:21 -0600

    init commit

---
+++ public/staff/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <header>GBI Staff Area</header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </navigation>
+
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
+
+  </body>
+</html>
